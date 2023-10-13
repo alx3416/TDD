029d7f95586f10ae29a0b9298611580daea7a60c
alx3416 <63263335+alx3416@users.noreply.github.com>
2023-10-13 14:23:34 -0600

    adding matplotlib examples

--- main.py
+++ main.py
@@ -1,26 +1,75 @@
-import utils.misc as ut
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ejemplo 1
+plt.plot([1, 2, 3, 4], [10, 20, 30, 40])
+plt.ylabel('some numbers')
+plt.show()
+
+plt.savefig('my_plot.png')
+
+# ejemplo 2
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+
+# ejemplo 3
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+plt.figure(figsize=(9, 3))
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+plt.show()
+
+# ejemplo 4
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
+plt.show()
+
+# ejemplo 5
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure()
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+
+# ejemplo 6
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, density=True, facecolor='g', alpha=0.75)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    foo = ut.get_hypotenuse(3, 4)
-    print(foo)
-
-    my_list = ut.create_sequence_list(0, 10)
-    new_list = ut.add_value_list(my_list, 4)
-    print(my_list)
-    print(new_list)
-
-    figure = ut.GeometricShape(3, 5)
-    print("número de lados de figura es: ", figure.sides)
-    print("perímetro de figura es: ", figure.get_perimeter())
-    circle = ut.Circle(7)
-    print("nombre de subfigura: ", circle.get_name())
-    print("área de círculo: ", circle.get_area())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
