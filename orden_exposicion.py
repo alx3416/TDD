ef116533c85084263c6719a92331f3a7af279ba1
alx3416 <alx3416@hotmail.com>
2023-11-21 11:31:08 -0600

    random value script

---
+++ orden_exposicion.py
@@ -0,0 +1,7 @@
+import random
+
+
+lista_equipos = ["C", "LOS KAYUS", "A", "SISTEMAS", "EL DANY", "REYNOSO", "B", "GPT", "LOS LUCHOS"]
+
+n = random.randint(0, len(lista_equipos)-1)
+print(lista_equipos[n])
